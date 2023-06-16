aefa9483bbd76811a60c0ea6f6eff0bd0de6d474
EcemGamze <134219985+EcemGamze@users.noreply.github.com>
2023-06-16 12:22:27 +0300

    JavaArtBook

---
+++ JavaArtBook/app/src/main/java/com/ecemgamzecuhadar/javaartbook/Art.java
@@ -0,0 +1,10 @@
+package com.ecemgamzecuhadar.javaartbook;
+
+public class Art {
+    String name;
+    int id;
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
